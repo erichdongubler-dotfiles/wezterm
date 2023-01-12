ffe341ec87512e9bb83d13ffbec66e27b1e9d66c
Erich Gubler <erichdongubler@gmail.com>
2023-01-11 21:20:27 -0500

    fix(font): disable ligatures

--- wezterm.lua
+++ wezterm.lua
@@ -3,4 +3,6 @@ local wezterm = require("wezterm")
 return {
 	default_prog = { "nu", "-l" },
 	font_size = 13.0,
+	-- Disable ligatures; I dun like 'em.
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 }
