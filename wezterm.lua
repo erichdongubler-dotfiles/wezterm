21ea157bf1c209b23327f8d28327f6500c7bc6b8
Erich Gubler <erichdongubler@gmail.com>
2023-01-19 10:09:01 -0500

    feat: show active workspace on right side of tab bar

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,9 @@
 local wezterm = require("wezterm")
 
+wezterm.on("update-right-status", function(window, pane)
+	window:set_right_status(window:active_workspace())
+end)
+
 return {
 	colors = {
 		foreground = "#CCCCCC",
